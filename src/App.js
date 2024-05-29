824101a6b4df7df8252cedaae4cd67e68fed5c4b
nila <nila@pdx.edu>
2024-05-28 21:50:28 -0700

    4 pages functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-// src/App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import StockList from './components/StockList';
 import StockPrice from './components/StockPrice';
 import News from './components/News';
+import SaveStock from './components/SaveStock';
+import StockPieChart from './components/StockPieChart';
 import './App.css';
 
 const App = () => {
@@ -15,7 +16,7 @@ const App = () => {
         <div className="top-bar">
           <h1>Stock Price Checker</h1>
           <nav>
-            <Link to="/">Home</Link> | <Link to="/news">News</Link>
+            <Link to="/">Home</Link> | <Link to="/news">News</Link> | <Link to="/save-stock">Save Stock</Link> | <Link to="/pie-chart">Pie Chart</Link>
           </nav>
         </div>
         <div className="content">
@@ -26,11 +27,13 @@ const App = () => {
                   <StockList onSelect={setSelectedSymbol} />
                 </div>
                 <div className="main-pane">
-                  <StockPrice selectedSymbol={setSelectedSymbol} />
+                  <StockPrice selectedSymbol={selectedSymbol} />
                 </div>
               </>
             } />
             <Route path="/news" element={<News />} />
+            <Route path="/save-stock" element={<SaveStock />} />
+            <Route path="/pie-chart" element={<StockPieChart />} />
           </Routes>
         </div>
       </div>
