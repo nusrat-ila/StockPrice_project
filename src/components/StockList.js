1a8959a9d633c5034e2aa3c85ed266d7329105ba
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-06-04 10:49:16 -0700

    Sort stocks within alphabetical groups

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,40 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import Switch from 'react-switch';
 import Collapsible from 'react-collapsible';
-
-// let sort_mode = 'name';
-// let checked = false;
-
-/* Return toggled state of the given sort mode:
-   'symbol' if it was 'name'
-   'name' if it was 'symbol'
-   'SortModeError' if it was none of these (should be treated as an error)
-*/
-// const toggleSortMode = () => {
-//   if (sort_mode === 'name') 
-//     sort_mode = 'symbol';
-//   else if (sort_mode === 'symbol')
-//     sort_mode = 'name';
-//   else
-//     sort_mode = 'sortModeError';
-// }
-
-//Component for the sort mode switch
-// const SortModeSwitch = () => {
-//   return (
-//     <label>
-//       <span>Name</span>
-//       <Switch onChange={toggleSortMode} checked={checked} />
-//       <span>Symbol</span>
-//     </label>
-//   )
-// }
 
 const getSymbolsInRange = (symbols, range) => {
   return symbols.filter((item) => {
     return item.symbol.match(range) ? true : false;
-  })
+  }).sort((a, b) => a.symbol.localeCompare(b.symbol))
 }
 
 const CollapsableStockSublist = ({ onSelect, symbols, trigger }) => {
