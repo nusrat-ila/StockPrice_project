8286ef71ec7cc9e0d0c34ba93b86686b9b821034
nila <nila@pdx.edu>
2024-06-04 21:36:22 -0700

    update the background image and work on some transparency of the content.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import StockList from './components/StockList';
-import StockPrice from './components/StockPrice';
-import News from './components/News';
-import SaveStock from './components/SaveStock';
-import StockPieChart from './components/StockPieChart';
+import { BrowserRouter as Router } from 'react-router-dom';
+import MainContent from './MainContent';
 import './App.css';
 
 const App = () => {
@@ -12,34 +8,7 @@ const App = () => {
 
   return (
     <Router>
-      <div className="App">
-        <div className="top-bar">
-          <h1>Stock Price Checker</h1>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/news">News</Link>
-            <Link to="/save-stock">Save Stock</Link>
-            <Link to="/pie-chart">Pie Chart</Link>
-          </nav>
-        </div>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={
-              <>
-                <div className="left-pane">
-                  <StockList onSelect={setSelectedSymbol} />
-                </div>
-                <div className="main-pane">
-                  <StockPrice selectedSymbol={selectedSymbol} />
-                </div>
-              </>
-            } />
-            <Route path="/news" element={<News />} />
-            <Route path="/save-stock" element={<SaveStock />} />
-            <Route path="/pie-chart" element={<StockPieChart />} />
-          </Routes>
-        </div>
-      </div>
+      <MainContent selectedSymbol={selectedSymbol} setSelectedSymbol={setSelectedSymbol} />
     </Router>
   );
 };
